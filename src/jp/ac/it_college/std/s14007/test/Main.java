550019fa0510abdb89d937458c150b0a80436525
s14007 <s14007@std.it-college.ac.jp>
2015-06-30 14:11:27 +0900

    Initial commit

---
+++ src/jp/ac/it_college/std/s14007/test/Main.java
@@ -0,0 +1,34 @@
+package jp.ac.it_college.std.s14007.test;
+
+import jp.ac.it_college.std.nakasone.lib.exam.Communicator;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scan = new Scanner(System.in);
+        System.out.print("名前を入力してください: ");
+        String name = scan.next();
+        Communicator comm = new Communicator((msg) -> {
+            System.out.println("> " + msg);
+        });
+        comm.start();
+
+        while (true) {
+            try {
+                String input = scan.next();
+                comm.send(name + ": " + input);
+                if (input.equals("q")) {
+                    System.out.println("終了");
+                    comm.finish();
+                    System.exit(0);
+                    break;
+                }
+            } catch (IOException var3) {
+                var3.printStackTrace();
+            }
+        }
+    }
+}
